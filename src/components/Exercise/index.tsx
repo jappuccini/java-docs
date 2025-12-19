bef2224dd51d8d657caff969f4c3269afbcf402b
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2025-12-19 22:23:26 +0100

    remove gitpod

--- src/components/Exercise/index.tsx
+++ src/components/Exercise/index.tsx
@@ -4,34 +4,18 @@ import TabItem from '@theme/TabItem';
 import Link from '@docusaurus/Link';
 import CodeBlock from '@theme/CodeBlock';
 
-export default function Exercise({ pullRequest, branchSuffix }): JSX.Element {
+export default function Exercise({ pullRequest, branchSuffix }): React.JSX.Element {
   return (
     <Tabs>
       <TabItem value="exercise" label="Exercise" default>
         <CodeBlock language="console">
           {`git switch exercises/${branchSuffix}`}
         </CodeBlock>
-        <Link
-          to={`https://gitpod.io/#https://github.com/jappuccini/java-exercises/blob/exercises/${branchSuffix}/Exercise.java`}
-        >
-          <img
-            alt="Open in Gitpod"
-            src="https://gitpod.io/button/open-in-gitpod.svg"
-          ></img>
-        </Link>
       </TabItem>
       <TabItem value="solution" label="Solution">
         <CodeBlock language="console">
           {`git switch solutions/${branchSuffix}`}
         </CodeBlock>
-        <Link
-          to={`https://gitpod.io/#https://github.com/jappuccini/java-exercises/blob/solutions/${branchSuffix}/Exercise.java`}
-        >
-          <img
-            alt="Open in Gitpod"
-            src="https://gitpod.io/button/open-in-gitpod.svg"
-          ></img>
-        </Link>
       </TabItem>
       <TabItem value="pullrequest" label="Pull Request">
         Alle  Änderungen zwischen der Aufgabe und der Lösung findest du im Pull
--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -4,34 +4,18 @@ import TabItem from '@theme/TabItem';
 import Link from '@docusaurus/Link';
 import CodeBlock from '@theme/CodeBlock';
 
-export default function Task({ pullRequest, branchSuffix }): JSX.Element {
+export default function Task({ pullRequest, branchSuffix }): React.JSX.Element {
   return (
     <Tabs>
       <TabItem value="task" label="task" default>
         <CodeBlock language="console">
           {`git switch task/${branchSuffix}`}
         </CodeBlock>
-        <Link
-          to={`https://gitpod.io/#https://github.com/jappuccini/java-exercises/blob/task/${branchSuffix}/Exercise.java`}
-        >
-          <img
-            alt="Open in Gitpod"
-            src="https://gitpod.io/button/open-in-gitpod.svg"
-          ></img>
-        </Link>
       </TabItem>
       <TabItem value="solution" label="Solution">
         <CodeBlock language="console">
           {`git switch solution/${branchSuffix}`}
         </CodeBlock>
-        <Link
-          to={`https://gitpod.io/#https://github.com/jappuccini/java-exercises/blob/solution/${branchSuffix}/Exercise.java`}
-        >
-          <img
-            alt="Open in Gitpod"
-            src="https://gitpod.io/button/open-in-gitpod.svg"
-          ></img>
-        </Link>
       </TabItem>
       <TabItem value="pullrequest" label="Pull Request">
         Alle  Änderungen zwischen der Aufgabe und der Lösung findest du im Pull
