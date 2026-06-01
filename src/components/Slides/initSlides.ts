cfb47c59fc69abc4208ba0c18a8a5396e9e6b699
SteffenLm <33038091+SteffenLm@users.noreply.github.com>
2026-06-01 17:55:37 +0200

    bump

--- src/components/Slides/initSlides.ts
+++ src/components/Slides/initSlides.ts
@@ -1,9 +1,9 @@
 export const defaultInitSlides = () => {
-  const Zoom = require('reveal.js/plugin/zoom/zoom.js');
-  const Notes = require('reveal.js/plugin/notes/notes.js');
-  const Search = require('reveal.js/plugin/search/search.js');
-  const Highlight = require('reveal.js/plugin/highlight/highlight.js');
-  const Reveal = require('reveal.js/dist/reveal.js');
+  const Zoom = require('reveal.js/plugin/zoom');
+  const Notes = require('reveal.js/plugin/notes');
+  const Search = require('reveal.js/plugin/search');
+  const Highlight = require('reveal.js/plugin/highlight');
+  const Reveal = require('reveal.js');
   new Reveal({
     plugins: [Zoom, Notes, Search, Highlight],
   }).initialize({ hash: true });
